9440c97a72cb452c3e50bb5a26c6d173a433a6ab
Marcos Roberto Silva <103574980+marcosr3000@users.noreply.github.com>
2022-05-31 08:25:58 -0300

    desafio to do list js

---
+++ 03_JavaScript/Aula6_desafio_to_do_list/assets/script.js
@@ -0,0 +1,16 @@
+function add() {
+    var todo = document.getElementById('field');
+    var list = document.getElementById('list');
+    var todo2 = todo.value
+
+    list.insertAdjacentHTML('beforeend', `
+    <input type="checkbox" id="${todo2}">
+    <label class="checkbox" for="${todo2}">
+    ${todo2}<br>
+    </label>
+    `)
+    
+    document.getElementById('field').value = '';
+
+
+}
