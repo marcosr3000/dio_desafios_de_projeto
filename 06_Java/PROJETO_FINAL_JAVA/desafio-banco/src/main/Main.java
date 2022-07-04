404c0d9e0b0ab7af9254b929fa7ad971b7cde733
Marcos Roberto Silva <103574980+marcosr3000@users.noreply.github.com>
2022-07-04 08:20:15 -0300

    projeto final java

---
+++ 06_Java/PROJETO_FINAL_JAVA/desafio-banco/src/main/Main.java
@@ -0,0 +1,33 @@
+package main;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        Cliente marcos = new Cliente();
+//        marcos.setNome("Marcos Roberto");
+        System.out.println("Digite o nome do cliente:");
+        marcos.setNome(scan.next());
+
+        System.out.println("Digite o valor que quer depositar na sua Conta Corrente:");
+        double dinheiroDepositar = scan.nextDouble();
+
+        Conta cc = new ContaCorrente(marcos);
+//        cc.depositar(500);
+        cc.depositar(dinheiroDepositar);
+
+        System.out.println("Digite o valor que quer transferir para sua Conta Poupança:");
+        double dinheiroTranferir = scan.nextDouble();
+
+        Conta poupanca = new ContaPoupanca(marcos);
+//        cc.transferir(100, poupanca);
+        cc.transferir(dinheiroTranferir, poupanca);
+
+        cc.imprimirExtrato();
+        poupanca.imprimirExtrato();
+    }
+}
